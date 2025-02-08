0b0a450c3294cbdda304a2408c276f900064c87a
Gregory Casamento <greg.casamento@gmail.com>
2025-02-07 22:04:48 -0500

    Initial Commit

---
+++ NSFormatter_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSFormatter_test
+//
+//  Created by Gregory John Casamento on 2/7/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSFormatter_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSFormatter_test
+//
+//  Created by Gregory John Casamento on 2/7/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
